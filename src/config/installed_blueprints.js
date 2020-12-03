99ab1ef0e86fcf16c123aa5cdebb471dc1498a3d
Crowdbotics <team@crowdbotics.com>
2020-12-03 14:22:22 +0000

    add package blueprint-react-native-settings-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'Settings181771', human_name: 'Settings', access_route: 'Settings181771'},
 
   // you can add more installed blueprints here
   // access route is the route nate given to navigator
